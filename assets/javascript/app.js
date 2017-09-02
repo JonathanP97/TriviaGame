f16c33cf7cee81786393e95481c9142aaea2b3d6
Jonathan Plata <jonathan_plata@ymail.com>
2017-09-02 01:05:37 -0700

    done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,17 @@
+var numCorrect = 0;
+var numWrong = 0;
+var unanswered = 0;
+var intervalId;
+
 $(window).on("load", function() {
 
-	var timeRemaining = 41;
-	var intervalId;
+	var timeRemaining = 21;
 	time();
 
 	function time() {
 
-		if(timeRemaining == 0) {
-			clearTimeout(intervalId);
+		if(timeRemaining === 0) {
+			checkAnswers();
 		} else {
 			timeRemaining--;
 			$("#spanTime").html(timeRemaining)
@@ -15,5 +19,77 @@ $(window).on("load", function() {
 		}
 	}
 
-})
+});
 
+function checkAnswers() {
+
+	clearTimeout(intervalId);
+	console.log("checkAnswerFunciton");
+
+	for(var i=1; i<6; i++) {
+		var val = $('input[name=answer'+ i+' ]:checked').val();
+
+		if(i == 1) {
+			console.log(val);
+			if(val == "answerB") {
+				numCorrect++;
+			} else {
+				numWrong++;
+			}
+		}
+
+		if(i == 2) {
+
+			if(val == "answerC") {
+				numCorrect++;
+			} else {
+				numWrong++;
+			}
+		}
+
+		if(i == 3) {
+
+			if(val == "answerA") {
+				numCorrect++;
+			} else {
+				numWrong++;
+			}
+		}
+		if(i == 4) {
+
+			if(val == "answerA") {
+				numCorrect++;
+			} else {
+				numWrong++;
+			}
+		}
+		if(i == 5) {
+
+			if(val == "answerB") {
+				numCorrect++;
+			} else {
+				numWrong++;
+			}
+		}
+	}
+
+	results();
+}
+
+function results() {
+	var q;
+	console.log("Correct: " + numCorrect);
+	console.log("Wrong: " +numWrong);
+	if(numCorrect===1) { 
+		q=" question"; 
+	} else {
+		q=" questions";
+	}
+
+	$(".spanCorrect").html("You got " + numCorrect + q + " correct!");
+	$(".spanWrong").html("and " + numWrong + " wrong");
+
+	$("button").css("display", "none");
+	$("#resultsBox").css("display", "inline");
+	$("#questionBox").css("display", "none");
+}
